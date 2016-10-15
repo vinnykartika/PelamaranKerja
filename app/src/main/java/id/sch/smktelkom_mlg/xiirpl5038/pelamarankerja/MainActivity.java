7d35ab9e6c6048af5997505c8ca111f2096743a9
SMK Telkom SP Malang <vinnykartika10@gmail.com>
2016-10-15 13:51:10 +0700

    Third commit: membuat dan menyambungkan variable dengan id di layout

--- app/src/main/java/id/sch/smktelkom_mlg/xiirpl5038/pelamarankerja/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xiirpl5038/pelamarankerja/MainActivity.java
@@ -2,12 +2,37 @@ package id.sch.smktelkom_mlg.xiirpl5038.pelamarankerja;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    EditText etKTP, etPhone;
+    Button butOK;
+    RadioButton rbM, rbF;
+    CheckBox cbID, cbEN, cbMA, cbOT;
+    Spinner spRe;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etKTP = (EditText) findViewById(R.id.editTextKTP);
+        etPhone = (EditText) findViewById(R.id.editTextPhone);
+        rbM = (RadioButton) findViewById(R.id.radioButtonM);
+        rbF = (RadioButton) findViewById(R.id.radioButtonF);
+        cbID = (CheckBox) findViewById(R.id.checkBoxID);
+        cbEN = (CheckBox) findViewById(R.id.checkBoxEN);
+        cbMA = (CheckBox) findViewById(R.id.checkBoxMA);
+        cbOT = (CheckBox) findViewById(R.id.checkBoxOT);
+        spRe = (Spinner) findViewById(R.id.spinnerRe);
+        butOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
     }
 }
